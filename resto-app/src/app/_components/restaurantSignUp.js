e223f85476fd959b1045c23a9b00c987b38f592c
rahulgarg55 <gargr010>
2024-11-02 12:56:44 +0530

    signup

--- resto-app/src/app/_components/restaurantSignUp.js
+++ resto-app/src/app/_components/restaurantSignUp.js
@@ -1,33 +1,82 @@
+import React, { useState } from "react";
 const RestaurantSignUp = () => {
-    return (
-      <>
-        <h3> SignUp</h3>
-        <div className="input-wrapper">
-          <input type="text" placeholder="Enter email id" className="input-field" />
-        </div>
-        <div className="input-wrapper">
-          <input type="password" placeholder="Enter password"className="input-field" />
-        </div>
-        <div className="input-wrapper">
-          <input type="password" placeholder="Confirm password"className="input-field" />
-        </div>
-        <div className="input-wrapper">
-          <input type="password" placeholder="Enter Restaurant Name"className="input-field" />
-        </div>
-        <div className="input-wrapper">
-          <input type="password" placeholder="Enter City "className="input-field" />
-        </div>
-        <div className="input-wrapper">
-          <input type="password" placeholder="Enter Full Address"className="input-field" />
-        </div>
-        <div className="input-wrapper">
-          <input type="password" placeholder="Enter Contact Number"className="input-field" />
-        </div>
-        <divc className="input-wrapper">
-          <button className="input-field">Sign Up</button>
-        </divc>
-      </>
-    );
-  };
-  export default RestaurantSignUp;
-  
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [contact, setContact] = useState("");
+  const [city, setCity] = useState(""); 
+  return (
+    <>
+      <h3> SignUp</h3>
+      <div className="input-wrapper">
+        <input
+          type="text"
+          placeholder="Enter email id"
+          className="input-field"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </div>
+      <div className="input-wrapper">
+        <input
+          type="password"
+          placeholder="Enter password"
+          className="input-field"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </div>
+      <div className="input-wrapper">
+        <input
+          type="password"
+          placeholder="Confirm password"
+          className="input-field"
+          value={confirmPassword}
+          onChange={(event) => setConfirmPassword(event.target.value)}
+        />
+      </div>
+      <div className="input-wrapper">
+        <input
+          type="password"
+          placeholder="Enter Restaurant Name"
+          className="input-field"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+      </div>
+      <div className="input-wrapper">
+        <input
+          type="password"
+          placeholder="Enter City "
+          className="input-field"
+          value={city}
+          onChange={(event) => setCity(event.target.value)}
+        />
+      </div>
+      <div className="input-wrapper">
+        <input
+          type="password"
+          placeholder="Enter Full Address"
+          className="input-field"
+          value={address}
+          onChange={(event) => setAddress(event.target.value)}
+        />
+      </div>
+      <div className="input-wrapper">
+        <input
+          type="password"
+          placeholder="Enter Contact Number"
+          className="input-field"
+          value={contact}
+          onChange={(event) => setContact(event.target.value)}
+        />
+      </div>
+      <div className="input-wrapper">
+        <button className="input-field">Sign Up</button>
+      </div>
+    </>
+  );
+};
+export default RestaurantSignUp;
