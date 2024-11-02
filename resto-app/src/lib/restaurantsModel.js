fcc02cde59bed5ee33df12d36bb1dfc6c4076b65
rahulgarg55 <gargr010>
2024-11-02 13:50:14 +0530

    schema

--- resto-app/src/lib/restaurantsModel.js
+++ resto-app/src/lib/restaurantsModel.js
@@ -3,7 +3,12 @@ const { default: mongoose } = require("mongoose");
 
 
 const restaurantModel = new mongoose.Schema({
-    name:String
+    name:String,
+    email:String,
+    password:String,
+    phone:String,
+    address:String,
+    city:String,
 });
 export const restaurantSchema=mongoose.models.restaurants 
 || mongoose.model("restaurants", restaurantModel);
