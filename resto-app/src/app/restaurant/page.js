2de0ed10e7256990291e177a948621067cd2f53a
rahulgarg55 <gargr010>
2024-11-02 11:09:10 +0530

    kjn

--- resto-app/src/app/restaurant/page.js
+++ resto-app/src/app/restaurant/page.js
@@ -1,12 +1,18 @@
+'use client'
 import RestaurantLogin from "../_components/restaurantLogin";
 import RestaurantSignUp from "../_components/restaurantSignUp";
+import { useState } from "react";   
 const Restaurant = ()=>{
+    const [login,setLogin] = useState(true);  
     return (
         <>
         <h1>Restaurant Login/SignUp Page</h1>
-       <RestaurantLogin/>
-       <RestaurantSignUp/>
-       <button>Already Have an Account? SignUp</button>  
+        {
+           login?<RestaurantLogin/>: <RestaurantSignUp/>
+        }
+       <button onClick={()=>setLogin(!login)}>
+        {login?"Don't Have an Account? SignUp":"Already have an account?"   }
+       </button>  
         </>
     )
 }
